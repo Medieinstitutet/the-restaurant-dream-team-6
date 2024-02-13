84546d724992286e3d1674070df5b34b4ff10745
Alinde Öst <ost.alinde@gmail.com>
2024-02-13 13:07:43 +0100

    made som styling and textchanges to contactpage

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,13 +1,12 @@
-import React from "react";
+
 import { ContactForm } from "../components/ContactForm";
 
 export const Contact = () => {
-      return (
-        <>
-        <h2>Kontakta oss</h2>
-        <div className="contact">
-            <ContactForm />
-        </div>
-        </>
-      );
-}
+  return (
+    <>
+      <div className="contact">
+        <ContactForm />
+      </div>
+    </>
+  );
+};
