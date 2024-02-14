d28d73d58e14469001d9296ab25678c38cb90443
Cristian Klasa <christianklasa@gmail.com>
2024-02-14 11:26:18 +0100

    Added booking component, confirmed and failed booking component, changed services.ts to api.ts

---
+++ src/components/ConfirmedBooking.tsx
@@ -0,0 +1,10 @@
+function ConfirmedBooking() {
+	return (
+		<>
+			<h3>Bordet är bokat</h3>
+			<p>Varmt välkommen!</p>
+		</>
+	);
+}
+
+export default ConfirmedBooking;
---
+++ src/components/FailedBooking.tsx
@@ -0,0 +1,10 @@
+function FailedBooking() {
+	return (
+		<>
+			<h3>Något gick fel</h3>
+			<p>Din bokning kunde inte genomföras. Var vänlig försök igen eller ring oss för att boka direkt.</p>
+		</>
+	);
+}
+
+export default FailedBooking;
