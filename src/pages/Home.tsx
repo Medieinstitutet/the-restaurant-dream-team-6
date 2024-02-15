e7dc81c767f22ee1986e0342987bd9cba8093293
Kyra Getachew <kyragetachew@gmail.com>
2024-02-15 13:12:15 +0100

    added an about us section and some style on homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,6 +8,11 @@ export const Home = () => {
     <h1>Skog</h1>
     <img className="restaurantpic" src={restaurantpic} alt="A picture of a restaurant" />
     </div>
+    <section className="text">
+        <h2>Om oss</h2>
+        <p>Skog erbjuder en mångsidig mening av mat, från lokala specialiteter till internationella klassiker. Maten är tillagad med hög kvalitet och särskilt uppmärksamhet ges till att maten är frisk och innehåller lokala råvaror. </p>
+        <p>Restaurangen är öppen för middag och kvällsmat, och bjuder också på en mängd olika drycker, från lokala öl till fina vin. Varmt välkomna till oss!</p>
+    </section>
     <StartPage/>
     </>
 }
